d9291d0a807c2a9eded8fea888c09a0b6796caa8
tmgr_k <destinationluna4@gmail.com>
2016-03-11 15:08:20 +0900

    バーを方向キーで動かせるように

---
+++ Assets/BarControler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarControler : MonoBehaviour {
+
+	private float accel = 1000.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//力を加える
+		this.GetComponent<Rigidbody> ().AddForce (
+			transform.right * Input.GetAxisRaw ("Horizontal") * accel,
+			ForceMode.Impulse);
+	}
+}
