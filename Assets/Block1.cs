22b7a02aebd945850a5923c724095c8398dfcc59
tmgr_k <destinationluna4@gmail.com>
2016-03-11 17:05:55 +0900

    消えるブロックを配置

---
+++ Assets/Block1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision collision) {
+		//衝突判定
+		if (collision.gameObject.tag == "Ball") {
+			//相手のタグがBallならば、自分を消す
+			Destroy(this.gameObject);
+		}
+	}
+}
