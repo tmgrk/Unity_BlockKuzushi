525c55133de4ad1cdf1ecdba9b6bd575a1d83bbb
tmgr_k <destinationluna4@gmail.com>
2016-03-11 13:57:19 +0900

    ballにC#で初速を与えるところまで

---
+++ Assets/ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+
+	private float Speed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody>().AddForce(
+			(transform.forward + transform.right) * Speed,
+			ForceMode.VelocityChange);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
